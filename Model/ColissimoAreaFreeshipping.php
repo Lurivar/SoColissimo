0152a3b3b90e8d1e05d85440932b7fc0a5b492bf
Lurivar <lurivar@users.noreply.github.com>
2020-04-03 15:29:37 +0200

    models

---
+++ Model/ColissimoAreaFreeshipping.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ColissimoPickupPoint\Model;
+
+use ColissimoPickupPoint\Model\Base\ColissimoAreaFreeshipping as BaseColissimoAreaFreeshipping;
+
+class ColissimoAreaFreeshipping extends BaseColissimoAreaFreeshipping
+{
+
+}
---
+++ Model/ColissimoAreaFreeshippingQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ColissimoPickupPoint\Model;
+
+use ColissimoPickupPoint\Model\Base\ColissimoAreaFreeshippingQuery as BaseColissimoAreaFreeshippingQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'colissimo_area_freeshipping' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class ColissimoAreaFreeshippingQuery extends BaseColissimoAreaFreeshippingQuery
+{
+
+} // ColissimoAreaFreeshippingQuery
